a3ab84fa63386e145af53752bfecd9df253a9654
googoo81 <shw9052@naver.com>
2022-05-22 00:24:54 +0900

    js_console

--- 220519script.js
+++ 220519script.js
@@ -1,1 +1,12 @@
-alert("hi");
+// 콘솔에게 메세지를 보내는 코드
+const a = 9;
+const b = 3;
+const myName = "googoo81"; //JS는 camelcase방식으로 코드를 작성함
+
+console. log(a * b);
+console. log(a + b);
+console. log(a / b);
+console. log("hello " + myName);
+//작은 따옴표('')도 사용가능 주의사항은 시작한걸로 닫아야함
+//variable: 값을 저장하거나 유지하는 역할
+//constant: 상수(c에선 int)
