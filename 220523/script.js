fcf2e697572908a91360727ba23c91923fdee725
googoo81 <shw9052@naver.com>
2022-05-23 23:43:25 +0900

    220523_js

---
+++ 220523/script.js
@@ -0,0 +1,9 @@
+const amIFat = undefined;
+let something;
+console.log(something, amIFat);
+
+//js 자료형 
+// 1. true: 2진수의 1처럼 '참'과 '켜짐'을 의미함.
+// 2. flase: 2진수의 0처럼 '거짓'과 '꺼짐'을 의미함.
+// 3. null: '그냥'없음을 뜻함.
+// 4. undefined: 설정은 되었으나 내용물이 비엇음.
