41efdf34602a5856fd6eef7f3fca2cd31a3362e4
googoo81 <shw9052@naver.com>
2022-05-24 21:03:13 +0900

    js_220524

--- 220524/script.js
+++ 220524/script.js
@@ -10,3 +10,15 @@ daysOfWeek.push("sun");
 console.log(daysOfWeek);
 
 //array의 목적은 하나의 variable 안에 데이터의 list를 가지는 것
+
+const player = {
+    name: "googoo",
+    point: "81",
+    fat: true,
+};
+console.log(player);
+console.log(player.name);
+player.fat = false;
+player.newname = "tomato"; 
+player.point = player.point - 50;
+console.log(player);
