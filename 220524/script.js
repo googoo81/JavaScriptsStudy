982738c19cc58c885a729ae0f98dd153bdd12279
googoo81 <shw9052@naver.com>
2022-05-24 20:41:12 +0900

    js_220524

---
+++ 220524/script.js
@@ -0,0 +1,12 @@
+//데이터 구조 array
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+// Get Item From Array
+console.log(daysOfWeek);
+
+// Add one more day to the array
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek);
+
+//array의 목적은 하나의 variable 안에 데이터의 list를 가지는 것
