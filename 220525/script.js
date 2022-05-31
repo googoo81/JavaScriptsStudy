ce5e993c6739f57256f9c6c97550553a1105de60
googoo81 <shw9052@naver.com>
2022-05-31 10:54:56 +0900

    js_220531

--- 220525/script.js
+++ 220525/script.js
@@ -3,6 +3,9 @@ const age = parseInt(prompt("How old are you?"));
 if(isNaN(age)){
     console.log("Please write a number");
 }
+else if(age < 18) {
+    console.log("You are too young");
+}
 else {
-    console.log("Thank you for writing your age")
+    console.log("You can drink");
 }
