003b35ea1204f1bceb0bdbfe407b7280797b4f78
송현우 <101445027+googoo81@users.noreply.github.com>
2022-06-28 19:51:26 +0900

    broweser power

--- Momentum/app.js
+++ Momentum/app.js
@@ -1,8 +1,9 @@
 const loginInput = document.querySelector("#login-form input")
 const loginButton = document.querySelector("#login-form input")
 
-function onLoginBtnClick(){
-    console.log(loginInput.value);
+function onLoginBtnClick() {
+    const username = loginInput.value; 
+    console.log(username);
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
